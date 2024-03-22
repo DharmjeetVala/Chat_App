c8c282fb345c609b3d1dc635395d7892b8657abc
DharmjeetVala <dharmjeetvala@gmail.com>
2024-03-22 10:21:00 +0530

    Chat Activity UI

---
+++ Chat_Application/app/src/main/java/com/example/chat_application/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package com.example.chat_application.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.example.chat_application.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
